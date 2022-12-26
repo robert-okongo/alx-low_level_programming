cc8ebce50246354e36cf382554ca800a6656c28a
Robert_Okongo <91130968+robert-okongo@users.noreply.github.com>
2022-12-26 15:05:44 +0300

    Fifth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  * main - using a put function
  *
- * Return: 0 
+ * Return: 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
 
