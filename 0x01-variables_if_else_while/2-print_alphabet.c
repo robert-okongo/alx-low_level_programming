d072f07414457479c471fe8616482bace4868f05
root <robert.okongo@protonmail.com>
2022-12-27 16:23:16 -0800

    Commit three

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+	while
+		(letter <= 'z') {
+
+			putchar(letter);
+			letter++;
+		}
+	putchar('\n');
+	return (0);
+}
